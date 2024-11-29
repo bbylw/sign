031b207fa9bad48b5375c7fd6a657f85224ca45e
free <30594873+bbylw@users.noreply.github.com>
2024-11-29 23:40:52 +0800

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,11 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   build: {
-    outDir: 'dist'
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: './index.html',
+      },
+    }
   }
 }) 
